1740cd3b81f0f1dfab9a50eb8536e0249024a296
Lex Li <support@lextm.com>
2025-11-14 04:22:34 -0500

    chore: add UTF-8 encoding settings to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 npm install
 npm run build
+
+# Set UTF-8 encoding
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export RUBYOPT=-Eutf-8
+
+bundle install
 bundle update
 JEKYLL_ENV=production bundle exec jekyll build
